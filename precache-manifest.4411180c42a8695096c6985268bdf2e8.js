50c22e4f486574da6fca717dd6cb884f5b57ada8
Moshe Melamed <melamed2@gmail.com>
2020-02-06 16:53:29 +0200

    Updates

---
+++ precache-manifest.4411180c42a8695096c6985268bdf2e8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ada2d61fc1b9211a7cf8890ac42a802",
+    "url": "/WeatherApp/index.html"
+  },
+  {
+    "revision": "9ab87a592225c49c3caa",
+    "url": "/WeatherApp/static/css/main.9df3617f.chunk.css"
+  },
+  {
+    "revision": "b8b0c08c54307e2d3af1",
+    "url": "/WeatherApp/static/js/2.5e1a30aa.chunk.js"
+  },
+  {
+    "revision": "9ab87a592225c49c3caa",
+    "url": "/WeatherApp/static/js/main.a524e2ca.chunk.js"
+  },
+  {
+    "revision": "112a84b43865339c867f",
+    "url": "/WeatherApp/static/js/runtime~main.7470692f.js"
+  },
+  {
+    "revision": "b25154f00336ae1fae359cbec0b28973",
+    "url": "/WeatherApp/static/media/Alef-Bold.b25154f0.ttf"
+  },
+  {
+    "revision": "c5d54929c418e69868b42a4d75063958",
+    "url": "/WeatherApp/static/media/Alef-Regular.c5d54929.ttf"
+  },
+  {
+    "revision": "15e8d135e564df6c570ec713faea4952",
+    "url": "/WeatherApp/static/media/Signika-Light.15e8d135.ttf"
+  },
+  {
+    "revision": "ba0080fada3310c48a0983c0a081db8b",
+    "url": "/WeatherApp/static/media/Signika-Regular.ba0080fa.ttf"
+  }
+]);
